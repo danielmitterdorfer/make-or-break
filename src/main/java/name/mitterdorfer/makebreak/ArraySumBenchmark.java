1815ce6c73dceb62936649ae6fe19823085ccc83
Daniel Mitterdorfer <daniel.mitterdorfer@comsysto.com>
2015-03-14 21:17:56 +0100

    Use a more fine-grained approach for demonstrating branch mispredictions

--- src/main/java/name/mitterdorfer/makebreak/ArraySumBenchmark.java
+++ src/main/java/name/mitterdorfer/makebreak/ArraySumBenchmark.java
@@ -2,60 +2,65 @@ package name.mitterdorfer.makebreak;
 
 import org.openjdk.jmh.annotations.*;
 
-import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Based on http://stackoverflow.com/questions/11227809/why-is-processing-a-sorted-array-faster-than-an-unsorted-array
+ * Loosely based on http://stackoverflow.com/questions/11227809/why-is-processing-a-sorted-array-faster-than-an-unsorted-array
  *
- * This benchmark demonstrates the effects of the branch prediction unit. The method <code>#benchmarkSumUnsorted()</code>
- * will produce lots of branch mispredictions due to random data in the array whereas <code>#benchmarkSumSorted()</code>
- * will behave much better because the branches are easy to predict due to sorted data:
- *
- * Benchmark                                        Mode  Samples     Score  Score error   Units
- * n.m.m.ArraySumBenchmark.benchmarkSumSorted      thrpt      200  1186,548        5,305  ops/us
- * n.m.m.ArraySumBenchmark.benchmarkSumUnsorted    thrpt      200   378,887        2,352  ops/us
+ * This benchmark demonstrates the effects of the branch prediction unit. Based on the benchmark parameter
+ * <code>randomizationProbability</code> the benchmark will produce more or less branch mispredictions due to
+ * random data in the array.
  */
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.Throughput)
-@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class ArraySumBenchmark {
-    private static final int PROBLEM_SIZE = 64 * 1024;
+    private static final int MAX_ELEMENT_VALUE = 256;
 
-    private final int[] unsortedArray = new int[PROBLEM_SIZE];
-    private final int[] sortedArray = new int[PROBLEM_SIZE];
+    private int[] array;
+
+    @Param({"8192", "16384", "32768", "65536"})
+    public int problemSize;
+
+    // every 0, 4, 2, 1 element is random
+    @Param({"0.0", "0.25", "0.5", "1.0"})
+    public double randomizationProbability;
 
     @Setup
     public void setUp() {
+        array = new int[problemSize];
+
         Random random = new Random(System.currentTimeMillis());
-        for (int idx = 0; idx < unsortedArray.length; idx++) {
-            unsortedArray[idx] = random.nextInt() % 256;
-        }
-        // create a sorted copy
-        System.arraycopy(unsortedArray, 0, sortedArray, 0, unsortedArray.length);
-        Arrays.sort(sortedArray);
-    }
-
-    @Benchmark
-    @OperationsPerInvocation(PROBLEM_SIZE)
-    public long benchmarkSumUnsorted() {
-        long sum = 0;
-        for (int idx = 0; idx < unsortedArray.length; idx++) {
-            if (unsortedArray[idx] >= 128) {
-                sum += unsortedArray[idx];
+        for (int idx = 0; idx < array.length; idx++) {
+            int value = getLinearValue(idx);
+            if (shouldBeRandomElement(idx)) {
+                // use a random value to break regularity in the pattern and to trip branch prediction
+                value = random.nextInt() % MAX_ELEMENT_VALUE;
             }
+            array[idx] = value;
         }
-        return sum;
+    }
+
+    private int getLinearValue(int idx) {
+        double percentageOfListSize = (double) idx / (double) (problemSize - 1);
+        return (int) Math.round(percentageOfListSize * MAX_ELEMENT_VALUE);
+    }
+
+    private boolean shouldBeRandomElement(int idx) {
+        if (randomizationProbability == 0.0d) {
+             return false;
+        }
+        int elementRange = (int) Math.round(1 / randomizationProbability);
+        return idx % elementRange == 0;
     }
 
     @Benchmark
-    @OperationsPerInvocation(PROBLEM_SIZE)
-    public long benchmarkSumSorted() {
+    public long benchmarkSum() {
         long sum = 0;
-        for (int idx = 0; idx < sortedArray.length; idx++) {
-            if (sortedArray[idx] >= 128) {
-                sum += sortedArray[idx];
+        for (int idx = 0; idx < array.length; idx++) {
+            if (array[idx] >= 128) {
+                sum += array[idx];
             }
         }
         return sum;
